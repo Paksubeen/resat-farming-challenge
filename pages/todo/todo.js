c4f53e90dcd7c0d1c947ae9b3070fcba82aa6c54
Paksubeen <tnqls520@gmail.com>
2023-10-25 23:23:28 +0900

    ✨ 할 일 분류 기능 구현 #3

--- pages/todo/todo.js
+++ pages/todo/todo.js
@@ -5,6 +5,8 @@ const todoList = document.querySelector(".todo-list");
 
 let todoValue = todoInput.value;
 let priorityValue = priority.value;
+let list = [];
+let filter = "all";
 
 const handleTodoInput = (e) => {
   todoValue = e.target.value;
@@ -25,38 +27,92 @@ const handleAddTodo = () => {
     return;
   }
 
-  handleTodoList();
-  handleResetInput();
+  const item = {
+    id: list.length,
+    text: todoValue,
+    priority: priorityValue,
+    done: false,
+  };
+
+  list.push(item);
+  resetInput();
+  handleShowTodo(list);
 };
 
-const handleResetInput = () => {
+const resetInput = () => {
   todoValue = "";
   priorityValue = "";
   todoInput.value = "";
   priority.value = "";
 };
 
-const handleTodoList = () => {
-  const todoItem = document.createElement("li");
-  const checkBox = document.createElement("input");
-  const todoText = document.createElement("input");
-  const todoPriority = document.createElement("span");
+const handleCheckTodo = (id, checked) => {
+  const newList = list.map((item) =>
+    item.id === id ? { ...item, done: checked } : item
+  );
 
-  todoItem.className = "todo-item";
-  checkBox.className = "todo-check";
-  todoText.className = "todo-text";
-  todoPriority.className = "todo-priority";
+  list = newList;
 
-  checkBox.type = "checkBox";
-  todoText.value = todoValue;
-  todoPriority.textContent = priorityValue;
+  if (filter === "all") {
+    handleShowTodo(newList);
+  } else {
+    handleSortTodo(filter, newList);
+  }
+};
 
-  todoItem.appendChild(checkBox);
-  todoItem.appendChild(todoText);
-  todoItem.appendChild(todoPriority);
-  todoList.appendChild(todoItem);
+const handleShowTodo = (list) => {
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.firstChild);
+  }
+
+  list.forEach((item) => {
+    const todoItem = document.createElement("li");
+    const checkBox = document.createElement("input");
+    const todoText = document.createElement("input");
+    const todoPriority = document.createElement("span");
+
+    todoItem.className = "todo-item";
+    checkBox.className = "todo-check";
+    todoText.className = "todo-text";
+    todoPriority.className = "todo-priority";
+
+    checkBox.type = "checkbox";
+    checkBox.checked = item.done;
+    todoText.value = item.text;
+    todoPriority.textContent = item.priority;
+
+    checkBox.addEventListener("click", () =>
+      handleCheckTodo(item.id, checkBox.checked)
+    );
+
+    todoItem.appendChild(checkBox);
+    todoItem.appendChild(todoText);
+    todoItem.appendChild(todoPriority);
+    todoList.appendChild(todoItem);
+  });
 };
 
 todoInput.addEventListener("input", handleTodoInput);
 priority.addEventListener("change", handlePriority);
 addButton.addEventListener("click", handleAddTodo);
+
+const allButton = document.querySelector(".all-button");
+const doneButton = document.querySelector(".done-button");
+const notDoneButton = document.querySelector(".notDone-button");
+
+const showAllTodo = () => {
+  filter = "all";
+  handleShowTodo(list);
+};
+
+const handleSortTodo = (state, list) => {
+  filter = state;
+
+  const newList = list.filter((item) => item.done === state);
+
+  handleShowTodo(newList);
+};
+
+allButton.addEventListener("click", showAllTodo);
+doneButton.addEventListener("click", () => handleSortTodo(true, list));
+notDoneButton.addEventListener("click", () => handleSortTodo(false, list));
